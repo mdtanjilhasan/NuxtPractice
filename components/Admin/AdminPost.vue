e5ef85805129d9798123fa4024bd887c5237c88d
tanjilhasan <engineertanjil@gmail.com>
2020-06-09 09:52:39 +0600

    admin and public section divided and error page modifi

---
+++ components/Admin/AdminPost.vue
@@ -0,0 +1,51 @@
+<template>
+    <form @submit.prevent="onSave">
+        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+        <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+        <AppButton type="submit">Save</AppButton>
+        <AppButton 
+        type="button" 
+        style="margin-left:10px;"
+        btn-style="cancel"
+        @click="onCancel"
+        >Cancel</AppButton>
+    </form>
+</template>
+<script>
+import AppButton from '~/components/UI/AppButton'
+import AppControlInput from '~/components/UI/AppControlInput'
+export default {
+    props:{
+        post:{
+            type: Object,
+            required: false
+        }
+    },
+    data(){
+        return{
+            editedPost:this.post ? { ...this.post } : { // ... spread operator. get all data inside props and make and object
+                author:'',
+                title:'',
+                thumbnailLink:'',
+                content:''
+            }
+        }
+    },
+    methods:{
+        onSave(){
+            // save the post
+            console.log(this.editedPost);
+        },
+        onCancel(){
+            // navigate back
+            this.$router.push('/admin');
+        }
+    },
+    components:{
+        AppControlInput,
+        AppButton
+    }
+}
+</script>
--- components/UI/AppControlInput.vue
+++ components/UI/AppControlInput.vue
@@ -16,7 +16,7 @@
 
 <script>
 export default {
-  name: 'AppInputControl',
+  name: 'AppControlInput',
   props: {
     controlType: {
       type: String,
